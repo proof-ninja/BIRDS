97e806d6a5d60d195a99a3f6a7f77eaa92265503
Hiroshi <hiroshi-cl@proof-ninja.co.jp>
2024-04-13 23:46:45 +0900

    incrementalization step2 B

--- src/incrementalization.ml
+++ src/incrementalization.ml
@@ -65,6 +65,8 @@ type intermediate_program = RuleAbstractionSet.t PredicateMap.t
 
 type view_parameters = (bool list) PredicateMap.t
 
+type pararell_occurrence_counts = int PredicateMap.t
+
 type error =
   | UnexpectedHeadVarForm  of var
   | UnexpectedBodyVarForm  of var
@@ -264,19 +266,13 @@ let incrementalization_ast (ast: expr): (rule list, error) result =
 
       sort_rules ast.rules >>= fun sorted_predicate_definitions ->
 
-      (* let _ = print_endline @@ (sorted_predicate_definitions |>
-        List.map (fun (impred, _) ->
-          string_of_intermediate_predicate impred
-        ) |> String.concat ", ")
-      in *)
+      let is_view_predicate = function
+        | ImPred name -> name = view_name
+        | ImDeltaInsert _ -> false
+        | ImDeltaDelete _ -> false
+      in
 
       let has_anonymous_view_patermeters =
-
-        let is_view_predicate = function
-          | ImPred name -> name = view_name
-          | ImDeltaInsert _ -> false
-          | ImDeltaDelete _ -> false
-        in
 
         let swap_view_parameter_result_list (list: (unit, bool list) Either.t list): (unit, bool list list) Either.t =
           List.fold_left (fun a b -> match (a, b) with
@@ -358,11 +354,76 @@ let incrementalization_ast (ast: expr): (rule list, error) result =
         Either.is_right
       in
       
-      let is_linear_occurrence = false (* TODO *) in
+      let is_linear_occurrence =
 
-      (* let is_linear_view = has_anonymous_view_patermeters && is_linear_occurrence in *)
-      let is_linear_view = has_anonymous_view_patermeters in
+        let poc_of_predicate (counts: pararell_occurrence_counts) (predicate: intermediate_predicate): int =
+          if is_view_predicate predicate then
+            1
+          else
+            PredicateMap.find_opt predicate counts |> Option.value ~default:0
+        in
 
+        let poc_of_intermediate_clause (counts: pararell_occurrence_counts) (clause: intermediate_clause): int =
+          match clause with
+            | ImPositive (predicate, _) -> poc_of_predicate counts predicate
+            | ImNegative (predicate, _) -> min (poc_of_predicate counts predicate) 1
+            | ImEquation    _ -> 0
+            | ImNonequation _ -> 0
+            | ImConstTerm   _ -> 0
+        in
+
+        let poc_of_rule_abstraction (counts: pararell_occurrence_counts) (rule: rule_abstraction): int =
+          rule.body |>
+          List.map (poc_of_intermediate_clause counts) |>
+          List.fold_left max 0
+        in
+        
+        let poc_of_rule_abstraction_list (counts: pararell_occurrence_counts) (rule_list: rule_abstraction list): int =
+          rule_list |>
+          List.map(poc_of_rule_abstraction counts) |>
+          List.fold_left (+) 0
+        in
+        
+        let soc_of_intermediate_clause (counts: pararell_occurrence_counts) (clause: intermediate_clause): int =
+          match clause with
+            | ImPositive (predicate, _) -> min (poc_of_predicate counts predicate) 1
+            | ImNegative (predicate, _) -> poc_of_predicate counts predicate
+            | ImEquation    _ -> 0
+            | ImNonequation _ -> 0
+            | ImConstTerm   _ -> 0
+        in
+
+        let soc_of_rule_abstraction (counts: pararell_occurrence_counts) (rule: rule_abstraction): int =
+          rule.body |>
+          List.map (soc_of_intermediate_clause counts) |>
+          List.fold_left (+) 0
+        in
+        
+        let soc_of_rule_abstraction_list (counts: pararell_occurrence_counts) (rule_list: rule_abstraction list): int =
+          rule_list |>
+          List.map(soc_of_rule_abstraction counts) |>
+          List.fold_left max 0
+        in
+        
+        let process_predicate_definition (either: (unit, pararell_occurrence_counts) Either.t) (definition: predicate_definition): (unit, pararell_occurrence_counts) Either.t =
+          match either with
+            | Either.Left () -> Either.Left ()
+            | Either.Right counts ->
+              let (predicate, rule_set) = definition in
+              let rule_list = rule_set |> RuleAbstractionSet.to_list in
+              let soc = soc_of_rule_abstraction_list counts rule_list in
+
+              if soc <= 1 then
+                let poc = poc_of_rule_abstraction_list counts rule_list in
+                Either.right (PredicateMap.add predicate poc counts)
+              else
+                Either.left ()
+        in
+
+        List.fold_left process_predicate_definition (Either.right PredicateMap.empty) sorted_predicate_definitions |> Either.is_right
+      in
+
+      let is_linear_view = has_anonymous_view_patermeters && is_linear_occurrence in
 
       let linear_view_rules =
         let convert_term: term -> term = function
