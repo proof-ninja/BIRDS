16f1c90ec0126ffa5345f79de9b629146fcc0c82
cedretaber <cedretaber@gmail.com>
2024-11-20 07:55:35 +0900

    Add --timeout option.

--- bin/verify.ml
+++ bin/verify.ml
@@ -1,8 +1,28 @@
 open Birds
 
+type args = {
+  filename: string;
+  timeout: int;
+}
+
+let default_timeout = 180
+let default_args = {
+  filename = "";
+  timeout = default_timeout;
+}
+
+let rec parse_argv args = function
+  | [] -> args
+  | "--timeout" :: timeout :: tail ->
+    begin match int_of_string_opt timeout with
+    | Some timeout -> parse_options { args with timeout } tail
+    | None -> failwith @@ Printf.sprintf "Invalid timeout: %s" timeout
+    end
+  | filename :: tail -> parse_options { args with filename } tail
+
 let _ =
-  let filename = Sys.argv.(1) in
-  let chan = open_in filename in
+  let args = parse_argv default_args @@ List.tl @@ Array.to_list Sys.argv in
+  let chan = open_in args.filename in
   let lexbuf = Lexing.from_channel chan in
   let ast = Parser.main Lexer.token lexbuf in
-  Verify.verify true 600 ast
+  Verify.verify true args.timeout ast
