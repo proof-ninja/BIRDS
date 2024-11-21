55d44233d2bc8644e4545730f44d85564fa41c5b
cedretaber <cedretaber@gmail.com>
2024-11-22 01:24:54 +0900

    Use Arg module to parse args.

--- bin/verify.ml
+++ bin/verify.ml
@@ -1,28 +1,16 @@
 open Birds
 
-type args = {
-  filename: string;
-  timeout: int;
-}
-
-let default_timeout = 180
-let default_args = {
-  filename = "";
-  timeout = default_timeout;
-}
-
-let rec parse_argv args = function
-  | [] -> args
-  | "--timeout" :: timeout :: tail ->
-    begin match int_of_string_opt timeout with
-    | Some timeout -> parse_argv { args with timeout } tail
-    | None -> failwith @@ Printf.sprintf "Invalid timeout: %s" timeout
-    end
-  | filename :: tail -> parse_argv { args with filename } tail
+let usage_message = "Usage: verify [--timeout <timeout>] <filename>"
+let filename = ref ""
+let timeout = ref 180
+let parse_rest filename' = filename := filename'
+let speclist = [
+  ("--timeout", Arg.Set_int timeout, "Set the timeout in seconds");
+]
 
 let _ =
-  let args = parse_argv default_args @@ List.tl @@ Array.to_list Sys.argv in
-  let chan = open_in args.filename in
+  Arg.parse speclist parse_rest usage_message;
+  let chan = open_in !filename in
   let lexbuf = Lexing.from_channel chan in
   let ast = Parser.main Lexer.token lexbuf in
-  Verify.verify true args.timeout ast
+  Verify.verify true !timeout ast
